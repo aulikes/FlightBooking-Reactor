25f56d1aefd5f411de3035c24be8800ac066b64c
aulikes <aulikes@hotmail.com>
2025-06-07 16:02:48 -0500

    update Application

---
+++ src/main/java/com/aug/flightbooking/infrastructure/config/ApplicationProperties.java
@@ -0,0 +1,23 @@
+package com.aug.flightbooking.infrastructure.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+@Data
+public class ApplicationProperties {
+
+    private Redis redis;
+
+
+    // -----------------------------------------------
+    // Subclases anidadas
+    // -----------------------------------------------
+
+    @Data
+    public static class Redis {
+        private long reservationTimeoutSeconds;
+        private long periodFluxSeconds;
+    }
+
+}
