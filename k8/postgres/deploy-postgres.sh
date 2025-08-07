cdf334dd2c69790916d771d721ea6c834d88340a
aulikes <aulikes@hotmail.com>
2025-08-06 22:35:36 -0500

    update k8 postgres

--- k8/postgres/deploy-postgres.sh
+++ k8/postgres/deploy-postgres.sh
@@ -3,16 +3,16 @@
 # Namespace donde se desplegarán los recursos
 NAMESPACE="flightbooking-dev"
 
-# Eliminar recursos previos en caso de errores anteriores
-echo "Eliminando posibles recursos anteriores..."
-kubectl delete -f 5-service-postgres.yaml -n "$NAMESPACE" --ignore-not-found
-kubectl delete -f 4-deployment-postgres.yaml -n "$NAMESPACE" --ignore-not-found
-kubectl delete -f 3-configmap-postgres.yaml -n "$NAMESPACE" --ignore-not-found
-kubectl delete -f 2-secret-postgres.yaml -n "$NAMESPACE" --ignore-not-found
-kubectl delete -f 1-pvc-postgres.yaml -n "$NAMESPACE" --ignore-not-found
-kubectl delete -f 0-pv-postgres.yaml --ignore-not-found
-
-sleep 2
+## Eliminar recursos previos en caso de errores anteriores
+#echo "Eliminando posibles recursos anteriores..."
+#kubectl delete -f 5-service-postgres.yaml -n "$NAMESPACE" --ignore-not-found
+#kubectl delete -f 4-deployment-postgres.yaml -n "$NAMESPACE" --ignore-not-found
+#kubectl delete -f 3-configmap-postgres.yaml -n "$NAMESPACE" --ignore-not-found
+#kubectl delete -f 2-secret-postgres.yaml -n "$NAMESPACE" --ignore-not-found
+#kubectl delete -f 1-pvc-postgres.yaml -n "$NAMESPACE" --ignore-not-found
+#kubectl delete -f 0-pv-postgres.yaml --ignore-not-found
+#
+#sleep 2
 
 # Verificar si el namespace existe
 echo "Verificando si el namespace '$NAMESPACE' existe..."
